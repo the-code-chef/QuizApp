a1826b500347f41a2070f04b90de41731009d450
Deepanshu Sinha <107sinha@gmail.com>
2017-03-29 14:24:13 +0530

    designs done

--- app/src/main/java/com/example/deepanshu/quizapp/MainActivity.java
+++ app/src/main/java/com/example/deepanshu/quizapp/MainActivity.java
@@ -2,12 +2,41 @@ package com.example.deepanshu.quizapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    private int scores = 0;
+    private int totalQuestion = 8;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void oneAnswered(View view) {
+    }
+
+    public void twoAnswered(View view) {
+
+    }
+
+    public void threeAnswered(View view) {
+    }
+
+    public void fourAnswered(View view) {
+    }
+
+    public void fiveAnswered(View view) {
+    }
+
+    public void sevenAnswered(View view) {
+    }
+
+    public void eightAnswered(View view) {
+    }
+
+    public void submitAnswer(View view) {
     }
 }
